d58045eb0a42052146a25f3bd4732f9615d8c60b
NesrinAytekin <NesrinAytekin@users.noreply.github.com>
2020-05-28 14:30:14 +0300

    Admin area created

--- Content_Managment_System/Startup.cs
+++ Content_Managment_System/Startup.cs
@@ -47,6 +47,11 @@ namespace Content_Managment_System
 
             app.UseEndpoints(endpoints =>
             {
+
+                endpoints.MapControllerRoute(
+                   name: "areas",
+                   pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
