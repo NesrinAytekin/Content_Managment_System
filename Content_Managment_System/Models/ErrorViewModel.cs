7feee6bd4a3773d24bada6d940b96f84e92da256
NesrinAytekin <NesrinAytekin@users.noreply.github.com>
2020-05-28 12:27:49 +0300

    Add project files.

---
+++ Content_Managment_System/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Content_Managment_System.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
