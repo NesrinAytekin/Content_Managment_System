86dc39a6a22587a9f5314b49c91a08b58d43db9e
NesrinAytekin <NesrinAytekin@users.noreply.github.com>
2020-05-28 12:49:51 +0300

    Installed sqlserver and tools packages and connection to database

---
+++ Content_Managment_System/Infrastructure/Context/ProjectContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Content_Managment_System.Infrastructure.Context
+{
+    public class ProjectContext:DbContext
+    {
+        public ProjectContext(DbContextOptions<ProjectContext> options):base(options) { }
+      
+    }
+}
---
+++ ReadMe.txt
@@ -0,0 +1,5 @@
+
+1.Package Manager aracýlýðý ile aþaðýdaki paketler yüklenir.
+	1.1.Microsoft.EntityFrameworkCore.SqlServer
+	1.2.Microsoft.EntityFrameworkCore.Tools
+2.Infrastructure
